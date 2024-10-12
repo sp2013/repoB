99d4eb5b7f6af165bcee8dfaab06862411094dfe
Suhas Patil <suhaspatil.29@gmail.com>
2024-10-12 11:26:37 +0530

    Create math_operations.h

---
+++ math_operations.h
@@ -0,0 +1,11 @@
+// Repo B: math_operations.h
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+#include "utils/utils.h"  // Using Repo C's header
+
+namespace math_operations {
+    int multiply_and_add(int a, int b, int c);
+}
+
+#endif
